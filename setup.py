e041f8c62dee15b79efba1322fc6c5c8257803d6
kanta-nakamura <knkmr0705@gmail.com>
2021-12-24 17:38:03 +0900

    [add] first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Generate a video title for Jaru Jaru Tower.',
+    author='kanta-nakamura',
+    license='MIT',
+)
