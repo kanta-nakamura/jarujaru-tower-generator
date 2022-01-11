2c579e7baad614b7ea17b3e2a4c5249ecfdf2ee7
kanta-nakamura <knkmr0705@gmail.com>
2022-01-11 17:52:10 +0900

    [update] 類似単語の導出

--- src/common/util.py
+++ src/common/util.py
@@ -63,3 +63,48 @@ def clip_grads(grads, max_norm):
     if rate < 1:
         for grad in grads:
             grad *= rate
+            
+
+def cos_similarity(x, y, eps=1e-8):
+    '''コサイン類似度の算出
+    :param x: ベクトル
+    :param y: ベクトル
+    :param eps: ”0割り”防止のための微小値
+    :return:
+    '''
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
+
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    '''類似単語の検索
+    :param query: クエリ（テキスト）
+    :param word_to_id: 単語から単語IDへのディクショナリ
+    :param id_to_word: 単語IDから単語へのディクショナリ
+    :param word_matrix: 単語ベクトルをまとめた行列。各行に対応する単語のベクトルが格納されていることを想定する
+    :param top: 上位何位まで表示するか
+    '''
+    if query not in word_to_id:
+        print('%s is not found' % query)
+        return
+
+    print('\n[query] ' + query)
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    vocab_size = len(id_to_word)
+
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(' %s: %s' % (id_to_word[i], similarity[i]))
+
+        count += 1
+        if count >= top:
+            return
