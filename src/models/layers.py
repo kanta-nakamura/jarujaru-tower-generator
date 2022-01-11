f219b2be0aeeae42c19f30f54cadc5b5bc769945
kanta-nakamura <knkmr0705@gmail.com>
2022-01-11 17:30:30 +0900

    [add] layers

---
+++ src/models/layers.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from src.common.functions import softmax, cross_entropy_error
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+    
+    def forward(self, x):
+        W, = self.params
+        out = np.dot(x, W)
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW
+        return dx
+    
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None  # softmaxの出力
+        self.t = None  # 教師ラベル
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # 教師ラベルがone-hotベクトルの場合、正解のインデックスに変換
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
+        return dx
