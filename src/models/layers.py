1edaaf89fc509d8e1de9d5cf297a3c12ac73e1fb
kanta-nakamura <knkmr0705@gmail.com>
2022-01-18 11:27:01 +0900

    [update] UnigramSampler 実装

--- src/models/layers.py
+++ src/models/layers.py
@@ -70,7 +70,8 @@ class Embedding:
         np.add.at(dW, self.idx, dout)
         
         return None
-    
+
+
 class EmbeddindDot:
     def __init__(self, W):
         self.embed = Embedding(W)
@@ -93,3 +94,44 @@ class EmbeddindDot:
         self.embed.backward(dtarget_W)
         dh = dout * target_W
         return dh
+
+
+class UnigramSampler:
+    def __init__(self, corpus, power, sample_size):
+        self.sample_size = sample_size
+        self.vocab_size = None
+        self.word_p = None
+
+        counts = collections.Counter()
+        for word_id in corpus:
+            counts[word_id] += 1
+
+        vocab_size = len(counts)
+        self.vocab_size = vocab_size
+
+        self.word_p = np.zeros(vocab_size)
+        for i in range(vocab_size):
+            self.word_p[i] = counts[i]
+
+        self.word_p = np.power(self.word_p, power)
+        self.word_p /= np.sum(self.word_p)
+
+    def get_negative_sample(self, target):
+        batch_size = target.shape[0]
+
+        if not GPU:
+            negative_sample = np.zeros((batch_size, self.sample_size), dtype=np.int32)
+
+            for i in range(batch_size):
+                p = self.word_p.copy()
+                target_idx = target[i]
+                p[target_idx] = 0
+                p /= p.sum()
+                negative_sample[i, :] = np.random.choice(self.vocab_size, size=self.sample_size, replace=False, p=p)
+        else:
+            # GPU(cupy）で計算するときは、速度を優先
+            # 負例にターゲットが含まれるケースがある
+            negative_sample = np.random.choice(self.vocab_size, size=(batch_size, self.sample_size),
+                                               replace=True, p=self.word_p)
+
+        return negative_sample
