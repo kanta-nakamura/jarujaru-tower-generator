a524d9392fa00327d467649a2ffdff74898a5126
kanta-nakamura <knkmr0705@gmail.com>
2022-01-18 12:51:14 +0900

    [update] GPUを使用する場合の処理を追加

--- src/models/layers.py
+++ src/models/layers.py
@@ -59,19 +59,20 @@ class Embedding:
         self.params = [W]
         self.grads = [np.zeros_like(W)]
         self.idx = None
-        
+
     def forward(self, idx):
         W, = self.params
         self.idx = idx
         out = W[idx]
         return out
-    
+
     def backward(self, dout):
         dW, = self.grads
         dW[...] = 0
-        
-        np.add.at(dW, self.idx, dout)
-        
+        if GPU:
+            np.scatter_add(dW, self.idx, dout)
+        else:
+            np.add.at(dW, self.idx, dout)
         return None
 
 
