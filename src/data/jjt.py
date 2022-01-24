665c3a65f662885a8f2d72aaa94d74685b4f03ed
kanta-nakamura <knkmr0705@gmail.com>
2022-01-24 10:11:23 +0900

    [update] trainとtestを出力できるように変更

--- src/data/jjt.py
+++ src/data/jjt.py
@@ -6,12 +6,17 @@ vocab_file = 'vocab.pkl'
 corpus_file = 'corpus.npy'
 dataset_dir = Path(__file__).resolve().parents[2] / 'data/processed'
 
-def load_data():
+def load_data(target='all', train_size=0.8):
 
     corpus_path = dataset_dir / corpus_file
     vocab_path = dataset_dir / vocab_file
 
     corpus = np.load(corpus_path)
+    
+    if target == 'train':
+        corpus = corpus[:int(len(corpus) * train_size)]
+    elif target == 'test':
+        corpus = corpus[int(len(corpus) * train_size):]
 
     with open(vocab_path, 'rb') as f:
         word_to_id, id_to_word = pickle.load(f)
